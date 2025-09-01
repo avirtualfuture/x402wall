bf58a2327f022f1fa4d96901e9e29a35f0f63a5e
avirtualfuture <118632766+avirtualfuture@users.noreply.github.com>
2025-09-01 16:52:43 -0300

    feat: add CORS headers for improved cross-origin resource sharing

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(process.cwd(),'public')))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
 
 
 
